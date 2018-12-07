c6abf4c791f9ade4e42ebda7b387a720dd85f92d
Kim HyunWoo <stories2@naver.com>
2018-12-07 10:06:31 +0900

    다운로드 링크 수정

--- public/assets/js/AppDefine.js
+++ public/assets/js/AppDefine.js
@@ -5,7 +5,7 @@ var LOG_LEVEL_WARN = 3;
 var LOG_LEVEL_ERROR = 4;
 
 var DEBUGGING_URL = ""
-// var DEBUGGING_URL = "/Firebase-Google-Drive/public" // <-- comment when you deploy this project
+var DEBUGGING_URL = "/Firebase-Google-Drive/public" // <-- comment when you deploy this project
 
 var ROUTE_DEFAULT_URL = "/#!/"
 var ROUTE_DRIVE_URL = "/#!/drive"
